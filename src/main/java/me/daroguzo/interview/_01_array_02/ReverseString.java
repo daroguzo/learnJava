8f89019d90073424f9b02698a2fb71c8acd55817
daroguzo <kjw2122@naver.com>
2021-08-26 22:32:12 +0900

    배열 문제2: 배열 뒤집기

--- src/main/java/me/daroguzo/interview/_01_array_02/ReverseString.java
+++ src/main/java/me/daroguzo/interview/_01_array_02/ReverseString.java
@@ -4,7 +4,7 @@ public class ReverseString {
 
     public static void main(String[] args) {
         ReverseString reverseString = new ReverseString();
-        System.out.println(reverseString.solution1("Hello".toCharArray()));
+        System.out.println(reverseString.solution3("Hello".toCharArray()));
     }
 
     /**
@@ -16,7 +16,14 @@ public class ReverseString {
      * @return
      */
     private char[] solution1(char[] message) {
-        return null;
+        char[] reversedChars = new char[message.length];
+
+        // 10 -> 0, 9 -> 1, 8 -> 2
+        for (int i = message.length - 1 ; i >= 0 ; i--) {
+            reversedChars[message.length - 1 - i] = message[i];
+        }
+
+        return reversedChars;
     }
 
     /**
@@ -27,7 +34,20 @@ public class ReverseString {
      * @param message
      * @return
      */
-    private char[] solution(char[] message) {
+    private char[] solution2(char[] message) {
+        for (int i = 0 ; i < message.length/2 ; i++) {
+            char temp = message[i];
+            message[i] = message[message.length - 1 - i];
+            message[message.length - 1 - i] = temp;
+        }
         return message;
+    }
+
+    private char[] solution3(char[] message) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = message.length - 1; i >= 0; i--) {
+            sb.append(message[i]);
+        }
+        return sb.toString().toCharArray();
     }
 }
