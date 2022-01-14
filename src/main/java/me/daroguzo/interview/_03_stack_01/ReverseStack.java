1465ca970212e023ae5f8742aefd076b272b34d8
daroguzo <kjw2122@naver.com>
2022-01-14 15:26:21 +0900

    스택 1: 스택 뒤집기(재귀)

--- src/main/java/me/daroguzo/interview/_03_stack_01/ReverseStack.java
+++ src/main/java/me/daroguzo/interview/_03_stack_01/ReverseStack.java
@@ -5,19 +5,44 @@ import java.util.Stack;
 public class ReverseStack {
 
     public static void main(String[] args) {
-        Stack<Integer> numbers = new Stack();
+        Stack<Integer> numbers = new Stack<>();
         numbers.push(1);
         numbers.push(2);
         numbers.push(3);
 
         System.out.println(numbers);
         ReverseStack reverseStack = new ReverseStack();
-        reverseStack.solution(numbers);
+        reverseStack.solution2(numbers);
         System.out.println(numbers);
     }
 
     // TODO 스택을 뒤집는 코드를 작성하라.
-    private void solution(Stack<Integer> stack) {
+    private Stack<Integer> solution(Stack<Integer> stack) {
+        Stack<Integer> reversedStack = new Stack<>();
+        while (!stack.isEmpty()) {
+            reversedStack.push(stack.pop());
+        }
+        return reversedStack;
+    }
+
+    private void solution2(Stack<Integer> stack) {
+        if (stack.isEmpty()) {
+            return;
+        }
+        int pop = stack.pop();
+        solution2(stack);
+        insertAtBottom(stack, pop);
+    }
+
+    private void insertAtBottom(Stack<Integer> stack, int number) {
+        if (stack.isEmpty()) {
+            stack.push(number);
+            return;
+        }
+
+        int temp = stack.pop();
+        insertAtBottom(stack, number);
+        stack.push(temp);
     }
 
 }
