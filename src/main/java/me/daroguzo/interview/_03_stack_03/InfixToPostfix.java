327c8aada04568430d3f1ff3246c2e4407c6c2a7
daroguzo <kjw2122@naver.com>
2022-01-15 16:09:03 +0900

    스택3-2: 중위연산식을 후위연산식으로 바꾸기

--- src/main/java/me/daroguzo/interview/_03_stack_03/InfixToPostfix.java
+++ src/main/java/me/daroguzo/interview/_03_stack_03/InfixToPostfix.java
@@ -1,5 +1,7 @@
 package me.daroguzo.interview._03_stack_03;
 
+import java.util.Stack;
+
 public class InfixToPostfix {
 
     public static void main(String[] args) {
@@ -12,8 +14,46 @@ public class InfixToPostfix {
     /**
      * TODO 인픽스를 포스트픽스로 변환하기
      */
+    private int precedence(char c) {
+        if (c == '+' || c == '-') {
+            return 1;
+        } else if (c == '*' || c == '/') {
+            return 2;
+        }
+
+        return 0;
+    }
+
     private String convert(String infix) {
-        return infix;
+        infix = infix.trim();
+        StringBuilder result = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+        for (char c : infix.toCharArray()) {
+            if (Character.isDigit(c)) {
+                result.append(c);
+            } else if (c == '(') {
+                stack.push(c);
+            } else if (c == ')') {
+                while (!stack.isEmpty() && stack.peek() != '(') {
+                    result.append(stack.pop());
+                }
+                stack.pop();
+            } else {
+                while (!stack.isEmpty() && precedence(c) <= precedence(stack.peek())) {
+                    result.append(stack.pop());
+                }
+                stack.push(c);
+            }
+        }
+
+
+        while (!stack.isEmpty()) {
+            if (stack.peek() == '(') {
+                throw new IllegalArgumentException("Illegal infix expression");
+            }
+            result.append(stack.pop());
+        }
+        return result.toString();
     }
 
 }
